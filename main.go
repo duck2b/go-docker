40575472a9ac0802ccc47d88ee74083fee41a8be
张春博 <zhangchunbo@blued.com>
2022-01-28 10:55:00 +0800

    add param

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 )
 
-func SayHello(w http.ResponseWriter, r *http.Request)  {
+func SayHello(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
-	fmt.Fprintf(w,"hello world " + name)
+	age := r.URL.Query().Get("age")
+	fmt.Fprintf(w, "hello world "+name+": age ="+age)
 }
 
-func main()  {
+func main() {
 	http.HandleFunc("/", SayHello)
 
 	err := http.ListenAndServe(":8081", nil)
